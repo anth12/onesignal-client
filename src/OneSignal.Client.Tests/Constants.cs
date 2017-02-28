fe2ef246bb62c5c28284bbdab43b8d8d8f11d6c2
Anthony Halliday <anthony.halliday@outlook.com>
2017-02-28 12:48:31 +0100

    View apps unit tests

---
+++ src/OneSignal.Client.Tests/Constants.cs
@@ -0,0 +1,11 @@
+﻿
+namespace OneSignal.Client.Tests
+{
+    internal class Constants
+    {
+        internal const string ApiKey = "M2JiYmM4NzEtYTgxNy00NGU0LWIzODktMzFmNDI5YjRiODU5";
+
+        internal const string TestAppName = "Unit Test 1";
+        internal const string TestAppId = "c73dffd3-cfcf-414c-a407-7153860a4c47";
+    }
+}
---
+++ src/OneSignal.Client.Tests/V1/AppTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using OneSignal.Client.V1;
+using Xunit;
+
+namespace OneSignal.Client.Tests.V1
+{
+    public class AppTests
+    {
+        [Fact]
+        public async void Can_load_all_apps()
+        {
+            using (var client = new OneSignalClient(Constants.ApiKey))
+            {
+                var apps = await client.Apps.View();
+
+                apps.Should().NotBeEmpty();
+            }
+        }
+
+        [Fact]
+        public async void Can_load_app()
+        {
+            using (var client = new OneSignalClient(Constants.ApiKey))
+            {
+                var app = await client.Apps.View(Constants.TestAppId);
+
+                app.Name.Should().Be(Constants.TestAppName);
+            }
+        }
+    }
+}
